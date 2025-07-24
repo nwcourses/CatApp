afe99ed3933c0d2621031e3849e442512732a66d
Nick Whitelegg <nickw4426@gmail.com>
2025-07-24 16:55:40 +0100

    Add Cat class

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,8 @@
 fun main() {
-    println("Cat App")
+    val binnie = Cat("Binnie", 10, 10)
+    val clyde = Cat("Clyde", 5, 5)
+    binnie.walk()
+    clyde.walk()
+    println(binnie)
+    println(clyde)
 }
