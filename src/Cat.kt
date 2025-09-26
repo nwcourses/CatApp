1c1d18cac6e95fb1a37bdfb04de6a8c277fa21e6
Avi Sarkar <2sarka38@solent.ac.uk>
2025-09-26 11:26:02 +0100

    weight return

--- src/Cat.kt
+++ src/Cat.kt
@@ -8,6 +8,6 @@ class Cat (val name: String, var age: Int, var weight: Int) {
         weight += 1
     }
     override fun toString() : String {
-        return "Name: $name Age: $age"
+        return "Name: $name Age: $age Weight: $weight"
     }
 }
